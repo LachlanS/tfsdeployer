bca8db6fd026629745415228028c4b010f976ff5
jstangroome <none@none.com>
2010-03-19 05:10:30 +0000

    Updated command line build for VS2010RC.

--- Branches/Tfs2010/TfsDeployer/build.cmd
+++ Branches/Tfs2010/TfsDeployer/build.cmd
@@ -2,4 +2,4 @@
 setlocal
 if NOT "%1" == "" set target=/t:%1
 
-"%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe" TfsDeployer.msbuild /p:Configuration=Debug %target%
+"%windir%\Microsoft.NET\Framework\v4.0.30128\MSBuild.exe" TfsDeployer.msbuild /p:Configuration=Debug %target%
