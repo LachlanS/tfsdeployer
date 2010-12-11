595683719fcc8dd7c03fd63086599504b477fe23
jstangroome <none@none.com>
2010-12-11 23:51:03 +0000

    Fixed spelling error in ProcessTemplate property name.

--- Trunk/TfsDeployer/TfsDeployer/TeamFoundation/ProcessTemplate.cs
+++ Trunk/TfsDeployer/TfsDeployer/TeamFoundation/ProcessTemplate.cs
@@ -6,7 +6,7 @@ namespace TfsDeployer.TeamFoundation
     public class ProcessTemplate
     {
         public string Description { get; set; }
-        public string Patameters { get; set; }
+        public string Parameters { get; set; }
         public string ServerPath { get; set; }
         public string TeamProject { get; set; }
     }
